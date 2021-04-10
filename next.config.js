8bd94277deef0e5524c8772cabf177628d798069
Adriano Monecchi <fornacciari@gmail.com>
2021-04-10 16:47:18 -0300

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 //
 // Redirect set up
+// https://nextjs.org/docs/api-reference/next.config.js/redirects
 //
 module.exports = {
-  // Uncomment the line below to enable basePath, pages and
-  // redirects will then have a path prefix (`/app` in this case)
-  //
-  // basePath: '/app',
 
   // Force redirection to menudino
 
